f15b01d0a7f66b4039ef2723113a108bc2917a00
Nishchhal Prajapat <nishchhalprajapat@gmail.com>
2021-06-07 12:55:00 +0530

    create homepage and directory menu-item component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Homepage from './pages/homepage/homepage.componet'
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
